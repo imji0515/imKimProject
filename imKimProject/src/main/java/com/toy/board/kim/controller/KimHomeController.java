db042f9ac48dea6b98d63d7cac99c587b9c60265
kim3439 <kim3439@clipsoft.co.kr>
2022-06-05 17:46:17 +0900

    2022.06.05 수정 1 김성동 index.jsp 추가

--- imKimProject/src/main/java/com/toy/board/kim/controller/KimHomeController.java
+++ imKimProject/src/main/java/com/toy/board/kim/controller/KimHomeController.java
@@ -3,16 +3,17 @@ package com.toy.board.kim.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class KimHomeController {
-	@RequestMapping(value = "/KimHome", method=RequestMethod.GET)
-	@ResponseBody
-	public String goHome(HttpServletRequest request) {
-		System.out.println("enter");
-		return "Hello Spring Boot";
-	}
+	@RequestMapping(value = "/KimHome", method = RequestMethod.GET)
+    public String home() {
+		System.out.println("home controller start");
+                   
+        return "kim/index";
+    }
 }
